b327b294967363f99907cadecd0a0a68316b2248
Mingjie Li <limingjie@outlook.com>
2025-04-23 11:45:36 -0700

    Update schematic to latch and input

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # CH32V003 Soft Latching Power Circuits
 
-The Soft Latching Power Circuits is suitable for battery powered applications, there is no power consumption after powered off.
+The Soft Latching Power Circuit is ideal for battery-powered applications, as it draws no current once the system is powered down.
 
 ## Schematic
 
-- `R1` and `C1` keep `Q1` off, `R2` keeps `Q2` off.
-- When button `SW1` is pushed, the gate of `Q1` is pulled low and power on MCU. The MCU immediately pull `PA1` high to latch Q2 and Q1.
-- To power off the circuit, pull `PA1` low to turn off `Q1` and `Q2`.
-
 ![Alt text](schematic/CH32V003_Soft_Latching/CH32V003_Soft_Latching.png)
+
+- `R1` and `C1` hold `Q1` in the off state, while `R2` keeps `Q2` turned off.
+- Pressing the button `SW1` pulls the gate of `Q1` low, allowing power to reach the MCU.
+- Once powered, the MCU configures `PC7` as an input with the internal pull-up resistor, which latches both `Q1` and `Q2` on.
+- The MCU can also monitor the state of the button through pin `PC7`.
+- To shut down the circuit, the MCU configures `PC7` as an input with the internal pull-down resistor, turning off both `Q1` and `Q2`.
+
 
 ## Notes
 
-The circuit works perfectly using CH32V003!
+The circuit works flawlessly with the CH32V003!
 
-However, the first time I tried the circuit was using a CH552, it never works stably, it is probably because the CH552 GPIOs have internal pull-up resistor enabled in default state (Quasi-bidirectional mode (standard 8051), open-drain output, support input, pin has pull-up resistor), thus it may not fully turn `Q2` off. On the contrary, the CH32V003 GPIOs are in high impedance mode by default.
+Initially, I tested it with the CH552, but it never operated reliably. This is likely due to the CH552’s GPIOs defaulting to quasi-bidirectional mode (typical for 8051 cores), which includes an internal pull-up resistor. As a result, `Q2` may not be fully turned off. In contrast, the CH32V003 GPIOs default to high-impedance input mode, which avoids this issue.
 
 ## References
 
@@ -22,3 +25,4 @@ However, the first time I tried the circuit was using a CH552, it never works st
 - [CNLohr: ch32v003fun](https://github.com/cnlohr/ch32v003fun)
 - [A Guide to Debouncing](https://my.eng.utah.edu/~cs5780/debouncing.pdf)
 - [The simplest button debounce solution](https://www.e-tinkers.com/2021/05/the-simplest-button-debounce-solution/)
+- [Museum of the Game - Let's design some POKEY replacements](https://forums.arcade-museum.com/threads/lets-design-some-pokey-replacements.515774/post-4623716)
--- soft_latching.c
+++ soft_latching.c
@@ -10,36 +10,38 @@
  *  - GitHub: https://github.com/limingjie/
  */
 
-#include "ch32fun.h"
-// #include "ch32v003_GPIO_branchless.h"
 #include <stdio.h>
+#include "ch32fun.h"
 
-// Pins
-uint16_t led_pin    = PC1;
-uint16_t button_pin = PD0;
-
-int delay = 500;
+uint8_t  button_pin  = PC7;      // Latch and button pin
+uint8_t  blink_pin   = PD5;      // LED pin
+uint8_t  blink_state = FUN_LOW;  // LED on
+uint32_t blink_delay = 500;      // LED blink delay
 
 void systick_init(void)
 {
     SysTick->CTLR = 0;
     NVIC_EnableIRQ(SysTicK_IRQn);
-    SysTick->CMP  = FUNCONF_SYSTEM_CORE_CLOCK / 1000 * (delay * 2);  // delay x 2
+    SysTick->CMP  = FUNCONF_SYSTEM_CORE_CLOCK / 1000 * blink_delay;
     SysTick->CNT  = 0;
     SysTick->CTLR = SYSTICK_CTLR_STE | SYSTICK_CTLR_STIE | SYSTICK_CTLR_STCLK;
+}
+
+void disable_systick(void)
+{
+    NVIC_DisableIRQ(SysTicK_IRQn);
 }
 
 void SysTick_Handler(void) __attribute__((interrupt));
 void SysTick_Handler(void)
 {
     // SysTick maintains accurate 20ms period
-    SysTick->CMP += FUNCONF_SYSTEM_CORE_CLOCK / 1000 * (delay * 2);  // delay x 2
+    SysTick->CMP += FUNCONF_SYSTEM_CORE_CLOCK / 1000 * blink_delay;
     SysTick->SR = 0;
 
     // blink the LED
-    funDigitalWrite(led_pin, FUN_HIGH);
-    Delay_Ms(delay);
-    funDigitalWrite(led_pin, FUN_LOW);
+    funDigitalWrite(blink_pin, blink_state);
+    blink_state = (blink_state == FUN_LOW) ? FUN_HIGH : FUN_LOW;
 }
 
 int main()
@@ -53,7 +55,7 @@ int main()
     funDigitalWrite(button_pin, FUN_HIGH);  // Input pull-up
 
     // Init LED pin
-    funPinMode(led_pin, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
+    funPinMode(blink_pin, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
 
     systick_init();
 
@@ -61,37 +63,47 @@ int main()
 #define STATE_KEY_PRESS_DEBOUNCE   1
 #define STATE_WAIT_FOR_KEY_RELEASE 2
 #define STATE_KEY_RELEASE_DEBOUNCE 3
-#define DEBOUNCE_CHECK_INTERVAL    5  // 5ms
+#define DEBOUNCE_INTERVAL          5  // 5ms
 #define DEBOUNCE_STABLE_CYCLES     2  // 5ms x 2 = 10ms
 #define KEY_OFF                    0
 #define KEY_ON                     1
 
-    int key_debounce_state = STATE_WAIT_FOR_KEY_PRESS;
-    int key_last_status    = KEY_OFF;
-    int key_cycle_count    = 0;
-    int key_released       = 0;
+    uint8_t key_debounce_state = STATE_WAIT_FOR_KEY_PRESS;
+    uint8_t key_last_status    = KEY_OFF;
+    uint8_t key_cycle_count    = 0;
+    // uint8_t key_pressed        = 0;
+    uint8_t key_released = 0;
 
-    int led_state = 0;
+    uint8_t led_state = 0;
 
-    // Wait key release after power on.
+    // If the key is holding after power on, wait until it is released.
+    // TODO: There should be a more elegant way...
     if (funDigitalRead(button_pin) == FUN_LOW)
     {
         while (1)
         {
             if (funDigitalRead(button_pin) == FUN_HIGH)
             {
-                break;
+                Delay_Ms(DEBOUNCE_INTERVAL);
+                if (funDigitalRead(button_pin) == FUN_HIGH)
+                {
+                    break;
+                }
             }
-            Delay_Ms(DEBOUNCE_CHECK_INTERVAL);
+            Delay_Ms(DEBOUNCE_INTERVAL);
         }
     }
 
     // Key states
-    //  STATE_WAIT_FOR_KEY_PRESS  <------->  STATE_KEY_PRESS_DEBOUNCE
-    //             ^                                    |
-    //             |                                    |
-    //             |                                    V
-    // STATE_KEY_RELEASE_DEBOUNCE <-------> STATE_WAIT_FOR_KEY_RELEASE
+    // TODO: Create a library
+    //  +----------------------------+         +----------------------------+
+    //  |  STATE_WAIT_FOR_KEY_PRESS  | <-----> |  STATE_KEY_PRESS_DEBOUNCE  |
+    //  +----------------------------+         +----------------------------+
+    //                ^                                      |
+    //                |                                      V
+    //  +----------------------------+         +----------------------------+
+    //  | STATE_KEY_RELEASE_DEBOUNCE | <-----> | STATE_WAIT_FOR_KEY_RELEASE |
+    //  +----------------------------+         +----------------------------+
     while (1)
     {
         switch (key_debounce_state)
@@ -111,6 +123,7 @@ int main()
                 {
                     if (key_cycle_count >= DEBOUNCE_STABLE_CYCLES)
                     {
+                        // key_pressed        = 1;
                         key_debounce_state = STATE_WAIT_FOR_KEY_RELEASE;
                         // printf("STATE_WAIT_FOR_KEY_RELEASE\n");
                     }
@@ -124,9 +137,6 @@ int main()
             case STATE_WAIT_FOR_KEY_RELEASE:
                 if (funDigitalRead(button_pin) == FUN_HIGH)  // released
                 {
-                    // key_released       = 1;
-                    // key_debounce_state = STATE_WAIT_FOR_KEY_PRESS;
-
                     key_last_status    = KEY_OFF;
                     key_cycle_count    = 0;
                     key_debounce_state = STATE_KEY_RELEASE_DEBOUNCE;
@@ -152,11 +162,17 @@ int main()
                 break;
         }
 
+        // if (key_pressed)
+        // {
+        //     disable_systick();
+        //     funDigitalWrite(blink_pin, FUN_LOW);
+        // }
+
         if (key_released)
         {
             led_state++;
-            delay = (delay == 500) ? 100 : 500;
-            // printf("Key Released. LED status = %d, delay = %d\n", led_state, delay);
+            blink_delay = (blink_delay == 500) ? 100 : 500;
+            // printf("Key Released. LED status = %d, blink_delay = %d\n", led_state, blink_delay);
             if (led_state > 8)
             {
                 funDigitalWrite(button_pin, FUN_LOW);  // Input pull-down
@@ -168,6 +184,6 @@ int main()
             key_released = 0;
         }
 
-        Delay_Ms(DEBOUNCE_CHECK_INTERVAL);
+        Delay_Ms(DEBOUNCE_INTERVAL);
     }
 }
